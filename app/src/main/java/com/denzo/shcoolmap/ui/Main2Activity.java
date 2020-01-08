b69b398af27417462a2a354a0ace7eab8d96e518
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-08 10:17:50 +0100

    update

--- app/src/main/java/com/denzo/shcoolmap/ui/Main2Activity.java
+++ app/src/main/java/com/denzo/shcoolmap/ui/Main2Activity.java
@@ -2,6 +2,7 @@ package com.denzo.shcoolmap.ui;
 
 import android.os.Bundle;
 
+import com.denzo.shcoolmap.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;

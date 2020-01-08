8a9fffb8ceb3eb82abadd35e6d4a820f118ce17f
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-08 08:34:21 +0100

    launch activity

--- app/src/main/java/com/denzo/shcoolmap/ui/home/HomeFragment.java
+++ app/src/main/java/com/denzo/shcoolmap/ui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,13 +29,20 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+
+
+
+        Button button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getContext(),MapsActivity.class);
+                startActivity(i);
+            }
+        });
         return root;
     }
-    public void button (View v){
 
-        Intent intent = new Intent(getActivity(), MapsActivity.class);
-        startActivity(intent);
-
-
-    }
 }

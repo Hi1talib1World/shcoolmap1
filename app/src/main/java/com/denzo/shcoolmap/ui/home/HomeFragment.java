1ec0e379c8432c411ea0dc13e6079e4ce35d1eb6
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-08 07:17:36 +0100

    update homefragment java

--- app/src/main/java/com/denzo/shcoolmap/ui/home/HomeFragment.java
+++ app/src/main/java/com/denzo/shcoolmap/ui/home/HomeFragment.java
@@ -23,7 +23,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        
+
         return root;
     }
 }
